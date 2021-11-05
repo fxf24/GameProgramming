9d0c4099af303f9e5aadcb2aab21afa64aec5208
Seunggyeom Kim <fxf24@naver.com>
2021-11-05 16:47:48 +0900

    Add Input practice source & Update README Input practice

--- C/03_Day/03_Day/02_Input.c
+++ C/03_Day/03_Day/02_Input.c
@@ -3,6 +3,14 @@
 
 int main()
 {
+	int year, month, date;
+	printf("고양이가 태어난 년도를 입력하세요 : ");
+	scanf("%d", &year);
+	printf("고양이가 태어난 달을 입력하세요 : ");
+	scanf("%d", &month);
+	printf("고양이가 태어난 일을 입력하세요 : ");
+	scanf("%d", &date);
+	printf("내 고양이는 %d 년 %d 월 %d 일에 태어났습니다. \n", year, month, date);
 
 	return 0;
 }
