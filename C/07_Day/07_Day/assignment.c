d32ca2bd859c99ddbcd33cb5e29317295245099f
Seunggyeom Kim <fxf24@naver.com>
2021-11-11 18:51:42 +0900

    Add assignment source & Update README assignment

--- C/07_Day/07_Day/assignment.c
+++ C/07_Day/07_Day/assignment.c
@@ -6,6 +6,19 @@ int main() {
 	printf("철수가 일한 시간을 입력하세요 : ");
 	scanf("%d", &time);
 
-	
+	if (time > 24) {
+		time = 24;
+	}
+	int salary;
+	if (time > 8) {
+		int ex = time - 8;
+		salary = 8 * 8720 + ex * 8720 * 1.5;
+	}
+	else {
+		salary = time * 8720;
+	}
+
+	printf("철수의 임금 :%d\n", salary);
+
 	return 0;
 }
