bdfbe47a4839bb0cad8fe4f80b27345a1b2055d3
Seunggyeom Kim <fxf24@naver.com>
2021-11-23 20:08:10 +0900

    Add assignment source

--- C/15_Day/15_Day/assignment.c
+++ C/15_Day/15_Day/assignment.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <stdlib.h>
 
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 int main()
 {
+	int arr[20];
+	srand(GetTickCount64());
 
+	for (int i = 0; i < 20; i++) {
+		arr[i] = rand() % 10 + 1;
+	}
+
+	for (int i = 0; i < 20; i++) {
+		printf("%d  ", arr[i]);
+	}
+	printf("\n");
+	printf("버블정렬을 시작합니다.\n");
+	system("pause");
+
+	for (int i = 0; i < 20; i++) {
+		for (int j = 1; j < 20-i; j++) {
+			if (arr[j - 1] > arr[j]) {
+				swap(&arr[j - 1], &arr[j]);
+				system("cls");
+				for (int i = 0; i < 20; i++) {
+					printf("%d  ", arr[i]);
+				}
+				printf("\n");
+				Sleep(100);
+			}
+		}
+	}
 	return 0;
 }
