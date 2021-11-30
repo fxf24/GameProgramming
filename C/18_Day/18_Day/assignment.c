5453283fd3429470bd06e424ad95d3bb9a10b883
Seunggyeom Kim <fxf24@naver.com>
2021-11-30 15:19:46 +0900

    Add assignment source

--- C/18_Day/18_Day/assignment.c
+++ C/18_Day/18_Day/assignment.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	// 2차원 배열을 동적할당 시켜주세요.
@@ -6,6 +7,19 @@ int main() {
 	// 2차원 배열의 길이는 Height = 4, width = 5 로 설정해주세요.
 	// 각 요소의 값은 원하시는 값으로 초기화 시켜주세요.
 	//
+	int* arr1 = (int*)calloc(20, sizeof(int));
 
+	int(*arr)[1] = &arr1;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			arr[i][j] = (i * 5) + j + 1;
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	free(arr1);
+	arr1 = NULL;
 	return 0;
 }
