2b6b737f90a8b979c14ad7607501085c339f7278
Seunggyeom Kim <fxf24@naver.com>
2021-12-09 17:51:29 +0900

    Update operator overload source

--- C++/OperatorOverload/Vector.h
+++ C++/OperatorOverload/Vector.h
@@ -29,4 +29,76 @@ public :
 		printf("[ %.1f, %.1f ] \n", X, Y);
 	}
 
+public :
+	// 연산자 오버로딩 방법
+	// 반환 타입 / operator"오버로딩할 연산자" / (피연산자)
+	// 반환 타입 : 연산이 완료된 후 나올 결과값으로 쓸 타입
+	// 피연산자  : 연산의 대상이 되는 데이터
+
+#pragma region Arithmetic Operator Overloading
+	FORCEINLINE Vector operator+(const Vector& value)
+	{
+		return Vector(X + value.X, Y + value.Y);
+	}
+
+	FORCEINLINE Vector operator+(const float& value)
+	{
+		return Vector(X + value, Y + value);
+	}
+
+	FORCEINLINE Vector operator-(const Vector& value)
+	{
+		return Vector(X - value.X, Y - value.Y);
+	}
+
+	FORCEINLINE Vector operator*(const Vector& value)
+	{
+		return Vector(X * value.X, Y * value.Y);
+	}
+#pragma endregion
+
+#pragma region Assignment Operator Overloading
+	FORCEINLINE Vector operator/(const Vector& value)
+	{
+		return Vector(X / value.X, Y / value.Y);
+	}
+
+	FORCEINLINE void operator+=(const Vector& value)
+	{
+		X += value.X;
+		Y += value.Y;
+	}
+
+	FORCEINLINE void operator-=(const Vector& value)
+	{
+		X -= value.X;
+		Y -= value.Y;
+	}
+
+	FORCEINLINE void operator*=(const Vector& value)
+	{
+		X *= value.X;
+		Y *= value.Y;
+	}
+
+	FORCEINLINE void operator/=(const Vector& value)
+	{
+		X /= value.X;
+		Y /= value.Y;
+	}
+#pragma endregion
+	
+#pragma region Increment, Decrement Operator Overloading
+	FORCEINLINE void operator++(const int value)
+	{
+		X++;
+		Y++;
+	}
+	FORCEINLINE void operator--(const int value)
+	{
+		X--;
+		Y--;
+	}
+#pragma endregion
+
 };
--- C++/OperatorOverload/main.cpp
+++ C++/OperatorOverload/main.cpp
@@ -2,9 +2,35 @@
 
 int main()
 {
-	Vector vec1 = Vector(1.f, 1.f);
+	Vector vec1 = Vector(1.f, 1.f) + Vector(1.f, 1.f);
 	vec1.PrintVector();
 
+	Vector vec2 = Vector(1.f, 1.f) + 1.5f;
+	vec2.PrintVector();
+
+	cout << "Vector +=" << endl;
+	vec1 += vec2;
+	vec1.PrintVector();
+
+	cout << "Vector -=" << endl;
+	vec1 -= vec2;
+	vec1.PrintVector();
+
+	cout << "Vector *=" << endl;
+	vec1 *= vec2;
+	vec1.PrintVector();
+
+	cout << "Vector /=" << endl;
+	vec1 /= vec2;
+	vec1.PrintVector();
+
+	cout << "Vector ++" << endl;
+	vec1++;
+	vec1.PrintVector();
+
+	cout << "Vector --" << endl;
+	vec1--;
+	vec1.PrintVector();
 
 	return 0;
 }
